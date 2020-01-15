e1a2433230977a4430f3d947b4c7a7177a5f1d6b
Roy <roy.appeldoorn@gmail.com>
2020-01-15 10:25:07 +0100

    Added test and sonarcloud .travis.yml config

--- src/test/java/com/mpsnake/api/ApiApplicationTests.java
+++ src/test/java/com/mpsnake/api/ApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.mpsnake.api;
 
+import com.mpsnake.api.controller.PlayerController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private PlayerController playerController;
 
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(playerController).isNotNull();
+    }
 }
