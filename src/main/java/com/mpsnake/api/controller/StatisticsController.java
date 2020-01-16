c5db2df0a8f596b74efcc08a15a1e00070db79e0
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-16 16:40:01 +0100

    Added Statistic resource mapping to the controller and added create method

--- src/main/java/com/mpsnake/api/controller/StatisticsController.java
+++ src/main/java/com/mpsnake/api/controller/StatisticsController.java
@@ -6,16 +6,11 @@ import com.mpsnake.api.model.Player;
 import com.mpsnake.api.model.Statistic;
 import com.mpsnake.api.repositories.StatisticsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("/statistics")
 @RestController
 public class StatisticsController {
     private final StatisticsRepository repository;
@@ -27,18 +22,21 @@ public class StatisticsController {
         this.repository = repository;
     }
 
-    @PostMapping("/statistics/{id}/kill")
+    @GetMapping("/{id}")
+    public Statistic getPlayerStatistics(@PathVariable String id) {
+        return statisticsLogic.getUserStatistics(id);
+    }
+
+    @PostMapping("/create/{id}")
+    public void createNewStatistic(@PathVariable String id) { statisticsLogic.createNewStatistic(id); }
+
+    @PostMapping("/{id}/kill")
     public void increaseKillCountForPlayer(@PathVariable String id) {
         statisticsLogic.increaseKillCountForPlayer(id);
     }
 
-    @PostMapping("/statistics/{id}/dead")
+    @PostMapping("/{id}/dead")
     public void increaseDeadCountForPlayer(@PathVariable String id) {
         statisticsLogic.increaseDeadCountForPlayer(id);
-    }
-
-    @GetMapping("/statistics/{id}")
-    public Statistic getPlayerStatistics(@PathVariable String id) {
-        return statisticsLogic.getUserStatistics(id);
     }
 }
