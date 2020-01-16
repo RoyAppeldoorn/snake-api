c1362896a2984e8f3184f4a078e29f2f342986f8
royappeldoorn <roy.appeldoorn@gmail.com>
2020-01-16 16:40:23 +0100

    Changed player_id to can not be empty

--- src/main/java/com/mpsnake/api/model/Player.java
+++ src/main/java/com/mpsnake/api/model/Player.java
@@ -3,17 +3,19 @@ package com.mpsnake.api.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 public class Player {
     @Id
     @JsonProperty("player_id")
+    @NotEmpty
     private String player_id;
 
     @JsonProperty("nickname")
+    @NotEmpty
     private String nickname;
 }
--- src/main/java/com/mpsnake/api/model/Statistic.java
+++ src/main/java/com/mpsnake/api/model/Statistic.java
@@ -2,15 +2,16 @@ package com.mpsnake.api.model;
 
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@Data
 public class Statistic {
 
     @Id
+    @NotEmpty
     private String player_id;
 
     @Column(nullable = true)
