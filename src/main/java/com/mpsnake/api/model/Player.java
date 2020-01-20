80cb1bf9d9a1b6d620b9de786d2fa730d2b81297
Roy <roy.appeldoorn@gmail.com>
2020-01-20 13:45:52 +0100

    Added getter/setter annotations

--- src/main/java/com/mpsnake/api/model/Player.java
+++ src/main/java/com/mpsnake/api/model/Player.java
@@ -2,12 +2,16 @@ package com.mpsnake.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.stereotype.Service;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Player {
     @Id
     @JsonProperty("player_id")
